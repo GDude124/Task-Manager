596a2145fcb46549aab46222d2172d257cfef2a3
Elijah Estes <elijahestes@Interapts-MacBook-Pro-6.local>
2018-03-14 16:12:11 -0400

    Finished task manager

---
+++ README.md
@@ -0,0 +1,2 @@
+# Task-Manager
+# filler-name
---
+++ Task Manager/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Task Manager
+//
+//  Created by Elijah Estes on 2/21/18.
+//  Copyright © 2018 Elijah Estes. All rights reserved.
+//
+
+import Foundation
+
+
+
